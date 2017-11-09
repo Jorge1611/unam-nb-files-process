6f9314982dccea6a87e92c874efccafc301c9e93
Jorge1611 <ingjorgevm@gmail.com>
2017-11-09 14:36:55 -0600

    Implementación IHubContext

---
+++ WA_UNAM_NB_PR/Hubs/ProcesorHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WA_UNAM_NB_PR.Hubs
+{
+    public class ProcesorHub : Hub
+    {
+
+    }
+}
