4faf50004dd2bc9fa7a852b24530ef56d3a1682d
Oscar Daniel Perez <odprz.dev@gmail.com>
2017-11-14 08:17:22 -0600

    feature-process-in-signal

--- WA_UNAM_NB_PR/Controllers/Services/ProcessController.cs
+++ WA_UNAM_NB_PR/Controllers/Services/ProcessController.cs
@@ -12,12 +12,54 @@ namespace WA_UNAM_NB_PR.Controllers.Services
 {
     public class ProcessController : ApiController
     {
-        // GET api/<controller>
+        
         [HttpGet]
-        [ActionName("go")]
-        public IEnumerable<string> Get()
+        [Route("api/process/go")]
+        public IHttpActionResult Go()
         {
-            return new string[] { "value1", "value2" };
+            var _ProcessManager = ProcessManager.Instance;
+            if (_ProcessManager.Status == 0)
+            {
+                _ProcessManager.Go();
+            }
+            return Ok(_ProcessManager.Status);
+        }
+
+    }
+
+    public class ProcessManager
+    {
+        private static readonly ProcessManager _instance = new ProcessManager();
+        public static ProcessManager Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        private Task _task;
+        public int Status;
+        private ProcessManager()
+        {
+            Debug.WriteLine("Inicio unico del constructor");
+            Status = 0;
+        }
+
+        /// <summary>
+        /// Lanza la tarea. Cambia el estatus a trabajando esperando 5 segundos y termina la tarea
+        /// </summary>
+        /// <returns> tarea </returns>
+        public Task Go()
+        {
+            _task = Task.Factory.StartNew(() =>
+            {
+                Status = 1;
+                Debug.WriteLine("Estatus = 1");
+                Thread.Sleep(5000);
+                Status = 0;
+                Debug.WriteLine("Estatus = 0");
+            });              
+            return _task;
         }
 
     }
